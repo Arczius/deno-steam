d91159fe0ca4aa85d68a7a7d98a123a8e31f4511
Arczius <70692175+Arczius@users.noreply.github.com>
2022-09-02 23:51:50 +0200

    made a few functions, and a few variables which call to the steam api

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # deno-steam
 a deno.js package / project to help you get started with using the steam api in a more userfriendly manner
+this uses async functions, so you need to use the await statement when getting data
--- mod.ts
+++ mod.ts
@@ -1,13 +1,36 @@
 "use strict";
 
 export default class DenoSteam{
-    apikey:string;
+    private apikey:string|number;
+    private steamurl= "http://api.steampowered.com" ;
 
     constructor(
-        apikey:string
+        apikey:string|number
     ){
         this.apikey = apikey;
     }
 
-    
+    public async GetNewsForApp(appid:string|number, amount:string|number, maxLength:string|number){
+        const response = await fetch(`${this.steamurl}/ISteamNews/GetNewsForApp/v0002/?appid=${appid}&count=${amount}&maxlength=${maxLength}`);
+        const json = await response.json().catch(console.error);
+        return await json;
+    }
+
+    public async GetGlobalAchievmentPercentagesForApp(appid:string|number){
+        const response = await fetch(`${this.steamurl}/ISteamUserStats/GetGlobalPercentagesForApp/v0002/?gameid=${appid}&format=json`);
+        const json = await response.json().catch(console.error);
+        return await json;
+    }
+
+    public async GetPlayerSummaries(playerid:string|number){
+        const response = await fetch(`${this.steamurl}/ISteamUser/GetPlayerSummaries/v0002/?key=${this.apikey}&steamids=${playerid}&format=json`);
+        const json = await response.json().catch(console.error);
+        return await json;
+    }
+
+    public async GetFriendList(playerid:string|number){
+        const response = await fetch(`${this.steamurl}/ISteamUser/GetFriendList/v0001/?key=${this.apikey}&steamid=${playerid}&relationship=friend&format=json`);
+        const json = response.json().catch(console.error);
+        return await json;
+    }
 }
